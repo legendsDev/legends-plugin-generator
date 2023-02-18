ee6ac566b4714120055e54c056c89ff8e75f8edb
legendsDev <witt@allthingsops.io>
2023-02-18 13:18:12 -0600

    initial commit

---
+++ extension.js
@@ -0,0 +1,160 @@
+const { readFileSync, readdirSync, mkdirSync, writeFileSync } = require('fs');
+const vscode = require('vscode');
+var extensionPath = "";
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	loadInputs();
+	
+	let disposable = vscode.commands.registerCommand('resource-generator-fivem.generate-resource', function (uri) {
+		loadInputs(uri.fsPath)
+	});
+	extensionPath = context.extensionPath + "/template"
+	
+	
+	context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+
+var InputData = {
+	pluginName : {
+		title : "Plugin Name",
+		placeholder : "holster, dropweapon, lowercasesignlename, ...",
+		nextInput : "description",
+		value : "",
+		input : null
+	},
+	description : {
+		title : "Plugin Description",
+		placeholder : "This plugin provides ...",
+		nextInput : "version",
+		value : "",
+		input : null
+	},
+	version : {
+		title : "Plugin Version",
+		placeholder : "1.0.0, ...",
+		nextInput : "",
+		value : "",
+		input : null
+	}
+}
+
+var startInput = "pluginName"
+
+
+function loadInputs(resource) {
+
+	for (const key in InputData) {
+
+		InputData[key].input = vscode.window.createInputBox();
+		
+		InputData[key].input.title = InputData[key].title;
+		InputData[key].input.placeholder = InputData[key].placeholder;
+	
+		InputData[key].input.onDidAccept(function() {
+			if (InputData[key].input.value.trim().length > 0) {
+				InputData[key].value = InputData[key].input.value
+
+				InputData[key].input.hide()
+
+				if (InputData[key].nextInput != "") {
+					InputData[InputData[key].nextInput].input.show()
+				} else {
+					startProcessing(resource)
+				}
+
+		
+			} else {
+				vscode.window.showErrorMessage(`You need to complete this input (${InputData[key].title}).`)
+			}
+
+		})
+	
+		if (startInput == key) {
+			InputData[key].input.show();
+		}
+
+	}
+	
+	
+}
+
+
+function startProcessing(url) {
+	var folder = getGeneratedFolder("");
+
+	
+	mkdirSync(`${url}/${InputData.pluginName.value}`)
+	writeFolder(`${url}/${InputData.pluginName.value}`, folder);
+
+	vscode.window.showInformationMessage(`You just created a new FiveM Lua Resource named '${InputData.pluginName.value}'.`)
+}
+
+function Folder(url) {
+	var folder = {
+		url : url,
+		files : {},
+		folders : {}
+	}
+
+	return folder
+}
+
+
+function getGeneratedFolder(url) {
+	var folder = Folder(url)
+
+	var files = readdirSync(extensionPath + url, { withFileTypes: true, encoding : 'utf-8' });
+
+		
+	for (const file of files) {
+		if (file.isDirectory()) {
+			folder.folders[file.name] = getGeneratedFolder(`${url}/${file.name}`)
+		} else {
+			folder.files[file.name] = readFileSync(`${extensionPath + url}/${file.name}`, {encoding : 'utf-8'})
+			
+			for (const key in InputData) {
+				folder.files[file.name] = folder.files[file.name].replace("${" + key + "}", InputData[key].value)
+			}
+		}
+	}
+
+	return folder;
+}
+
+
+
+function writeFolder(url, folder) {
+
+	url = url
+
+	for (const key in folder.files) {
+		writeFileSync(url + "/"+ key, folder.files[key], {encoding : 'utf-8'});
+	}
+
+
+	for (const key in folder.folders) {
+		var fold = folder.folders[key]
+		mkdirSync(url  + "/" + folder.url + fold.url)
+
+		writeFolder(url  + "/" + folder.url + fold.url, fold)
+	}
+
+}
+
+
+
+
+
+
+
+module.exports = {
+	activate,
+	deactivate
+}
